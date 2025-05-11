9d82494745d906a2be7f2058a77ba8974aac5b80
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2025-05-11 12:00:52 +0600

    adds crud services

---
+++ src/web/creature.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from model.creature import Creature
+from service.creature import service as creature_service
+
+router = APIRouter(prefix="/creature")
+
+@router.get("/")
+def get_all():
+    return creature_service.get_all()
+
+@router.get("/{name}")
+def get_one(name) -> Creature | None:
+    return creature_service.get_one(name)
+
+@router.post("/")
+def create_one(creature: Creature) -> Creature:
+    return creature_service.create_one(creature)
+
+@router.patch("/")
+def modify(creature: Creature) -> Creature:
+    return creature_service.modify(creature)
+
+@router.put("/")
+def replace(creature: Creature) -> Creature:
+    return creature_service.replace(creature)
+
+@router.delete("/{name}")
+def delete_one(name: str) -> bool:
+    return creature_service.delete_one(name)
--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,8 +1,30 @@
 from fastapi import APIRouter
+from model.explorer import Explorer
+from service.exporer import service as explorer_service
 
 router = APIRouter(prefix="/explorer")
 
 @router.get("/")
-def top():
-    return "top explorer endpoint"
+def get_all():
+    return explorer_service.get_all()
 
+@router.get("/{name}")
+def get_one(name) -> Explorer | None:
+    
+    return explorer_service.get_one(name)
+
+@router.post("/")
+def create_one(explorer: Explorer) -> Explorer:
+    return explorer_service.create_one(explorer)
+
+@router.patch("/")
+def modify(explorer: Explorer) -> Explorer:
+    return explorer_service.modify(explorer)
+
+@router.put("/")
+def replace(explorer: Explorer) -> Explorer:
+    return explorer_service.replace(explorer)    
+
+@router.delete("/{name}")
+def delete_one(name: str) -> bool:
+    return explorer_service.delete_one(name)
